89c5a9b606d31a36f93817bec9d5e9a1720af5a7
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-09-12 14:10:59 -0400

    Ads link to mongo and Create user button

---
+++ script.js
@@ -0,0 +1,30 @@
+async function createUserButtonClicked() {
+    const params = new URLSearchParams({email: "test", password: "test"});
+    const url = 'https://us-east-1.aws.data.mongodb-api.com/app/application-0-qvqcw/endpoint/AddEmployee?'+params.toString();
+    const apiKey = 'iOl4vy5hFzAvu9RGuStafQ39vqliO7qXAWsehKIh42IngWS8FZ57nvOjb71jpoJP';
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'bearer ' + apiKey,
+        },
+        params: {
+            email: "test",
+            password: "test"
+        }
+    };
+    
+    fetch(url, options)
+    .then(response => response.json())
+    .then(data => alert(data))
+    .catch(err => console.error(err));
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    const testMongoButton = document.getElementById('createUserButton');
+    if (testMongoButton) {
+        testMongoButton.addEventListener('click', function() {
+            createUserButtonClicked();
+        });
+    }
+});
