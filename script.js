c0226104f77cc0338a4296a4c641a0d08aa630f6
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-09-12 14:37:33 -0400

    Ads login capabilities

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-async function createUserButtonClicked() {
-    const params = new URLSearchParams({email: "test", password: "test"});
+const apiKey = 'iOl4vy5hFzAvu9RGuStafQ39vqliO7qXAWsehKIh42IngWS8FZ57nvOjb71jpoJP';
+
+async function employeeCreateUserButtonClicked() {
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const params = new URLSearchParams({email: email, password: password});
+
     const url = 'https://us-east-1.aws.data.mongodb-api.com/app/application-0-qvqcw/endpoint/AddEmployee?'+params.toString();
-    const apiKey = 'iOl4vy5hFzAvu9RGuStafQ39vqliO7qXAWsehKIh42IngWS8FZ57nvOjb71jpoJP';
     const options = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': 'bearer ' + apiKey,
-        },
-        params: {
-            email: "test",
-            password: "test"
         }
     };
     
@@ -21,10 +21,46 @@ async function createUserButtonClicked() {
 };
 
 document.addEventListener("DOMContentLoaded", function() {
-    const testMongoButton = document.getElementById('createUserButton');
+    const testMongoButton = document.getElementById('employeeCreateUserButton');
     if (testMongoButton) {
         testMongoButton.addEventListener('click', function() {
-            createUserButtonClicked();
+            employeeCreateUserButtonClicked();
+        });
+    }
+});
+
+async function EmployeeLoginButtonClicked() {
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const params = new URLSearchParams({email: email, password: password});
+
+    const url = 'https://us-east-1.aws.data.mongodb-api.com/app/application-0-qvqcw/endpoint/EmployeeLogIn?'+params.toString();
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'bearer ' + apiKey,
+        }
+    };
+    
+    fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.loggedIn) {
+            window.location.href = "employeePortal.html";
+        } else {
+            alert(JSON.stringify(data));
+        }
+    })
+    .catch(err => console.error(err));
+};
+
+document.addEventListener("DOMContentLoaded", function() {
+    const testMongoButton = document.getElementById('employeeLoginButton');
+    if (testMongoButton) {
+        testMongoButton.addEventListener('click', function() {
+            EmployeeLoginButtonClicked();
         });
     }
 });
