71530043c10e0434da6f54b9b96f9b65c91b2f87
JoeyThompson10 <108598229+JoeyThompson10@users.noreply.github.com>
2023-10-26 22:00:31 -0400

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -70,19 +70,32 @@ function LoginPage() {
         window.dispatchEvent(new Event('storageChange')); // Dispatch event after storage change
       }      
 
-    return (
-        <form onSubmit={EmployeeLoginButtonClicked}>
-            <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" required value={username} onChange={handleUsernameChange} />
+      return (
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-md-6 offset-md-3">
+                    <div className="card">
+                        <div className="card-body">
+                            <h2 className="text-center mb-4">Employee Login</h2>
+                            <form onSubmit={EmployeeLoginButtonClicked}>
+                                <div className="mb-3">
+                                    <label htmlFor="username" className="form-label">Username</label>
+                                    <input type="text" className="form-control" id="username" required value={username} onChange={handleUsernameChange} />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input type="password" className="form-control" id="password" required value={password} onChange={handlePasswordChange} />
+                                </div>
+                                <div className="d-grid gap-2">
+                                    <button type="submit" className="btn btn-primary">Login</button>
+                                    <button type="button" className="btn btn-link" onClick={forgotPassword}>Forgot Password?</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" required value={password} onChange={handlePasswordChange} />
-            </div>
-            <button type="submit">Login</button>
-            <button type="button" onClick={forgotPassword}>Forgot Password</button>
-        </form>
+        </div>
     );
 };
 
