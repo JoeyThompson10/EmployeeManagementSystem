fc3a88ee58fcd90bebf8343627c27dc23f378514
Joey Thompson <JoeyThompson10@gmail.com>
2023-12-04 20:24:52 -0500

    Ads bootstrap to ProfileManagement

--- src/components/ProfileManagement.jsx
+++ src/components/ProfileManagement.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { updateUserProfile, getEmployee } from '../MongoDbClient'; // Import the helper function
+import { updateUserProfile, getEmployee } from '../MongoDbClient';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const ProfileManagement = () => {
     const _id = localStorage.getItem('username');
@@ -21,7 +22,6 @@ const ProfileManagement = () => {
     const [emergencyContactState, setEmergencyContactState] = useState('');
     const [emergencyContactZipCode, setEmergencyContactZipCode] = useState('');
     const [emergencyContactCountry, setEmergencyContactCountry] = useState('');
-
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -72,70 +72,84 @@ const ProfileManagement = () => {
             emergencyContactZipCode,
             emergencyContactCountry
         );
-        // Handle the response (e.g., show a confirmation message)
         console.log(response);
         window.alert("Profile updated!");
     };
 
     return (
-        <div>
-            <h2>Profile Management</h2>
-            <form onSubmit={handleSubmit}>
-                {/* Form fields */}
-                <label>Email Address:
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </label>
-                <label>First Name:
-                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
-                </label>
-                <label>Last Name:
-                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                </label>
-                <label>Phone Number:
-                    <input type="tel" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
-                </label>
-                <label>Address:
-                    <input type="text" value={address} onChange={(e) => setAddress(e.target.value)} />
-                </label>
-                <label>City:
-                    <input type="text" value={city} onChange={(e) => setCity(e.target.value)} />
-                </label>
-                <label>State:
-                    <input type="text" value={state} onChange={(e) => setState(e.target.value)} />
+        <div className='container mt-5'>
+            <h2 className='text-center'>Profile Management</h2>
+            <form onSubmit={handleSubmit} className='mt-4'>
+                <div className="form-group">
+                    <label>Email Address:</label>
+                    <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>First Name:</label>
+                    <input type="text" className="form-control" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Last Name:</label>
+                    <input type="text" className="form-control" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Phone Number:</label>
+                    <input type="tel" className="form-control" value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Address:</label>
+                    <input type="text" className="form-control" value={address} onChange={(e) => setAddress(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>City:</label>
+                    <input type="text" className="form-control" value={city} onChange={(e) => setCity(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>State:</label>
+                    <input type="text" className="form-control" value={state} onChange={(e) => setState(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Zip Code:</label>
+                    <input type="text" className="form-control" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Country:</label>
+                    <input type="text" className="form-control" value={country} onChange={(e) => setCountry(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact Name:</label>
+                    <input type="text" className="form-control" value={emergencyContactName} onChange={(e) => setEmergencyContactName(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact Phone:</label>
+                    <input type="tel" className="form-control" value={emergencyContactPhone} onChange={(e) => setEmergencyContactPhone(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact Email:</label>
+                    <input type="email" className="form-control" value={emergencyContactEmail} onChange={(e) => setEmergencyContactEmail(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact Address:</label>
+                    <input type="text" className="form-control" value={emergencyContactAddress} onChange={(e) => setEmergencyContactAddress(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact City:</label>
+                    <input type="text" className="form-control" value={emergencyContactCity} onChange={(e) => setEmergencyContactCity(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact State:</label>
+                    <input type="text" className="form-control" value={emergencyContactState} onChange={(e) => setEmergencyContactState(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact Zip Code:</label>
+                    <input type="text" className="form-control" value={emergencyContactZipCode} onChange={(e) => setEmergencyContactZipCode(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label>Emergency Contact Country:</label>
+                    <input type="text" className="form-control" value={emergencyContactCountry} onChange={(e) => setEmergencyContactCountry(e.target.value)} />
+                </div>
 
-                </label>
-                <label>Zip Code:
-                    <input type="text" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
-                </label>
-                <label>Country:
-                    <input type="text" value={country} onChange={(e) => setCountry(e.target.value)} />
-                </label>
-                <label>Emergency Contact Name:
-                    <input type="text" value={emergencyContactName} onChange={(e) => setEmergencyContactName(e.target.value)} />
-                </label>
-                <label>Emergency Contact Phone:
-                    <input type="tel" value={emergencyContactPhone} onChange={(e) => setEmergencyContactPhone(e.target.value)} />
-                </label>
-                <label>Emergency Contact Email:
-                    <input type="email" value={emergencyContactEmail} onChange={(e) => setEmergencyContactEmail(e.target.value)} />
-                </label>
-                <label>Emergency Contact Address:
-                    <input type="text" value={emergencyContactAddress} onChange={(e) => setEmergencyContactAddress(e.target.value)} />
-                </label>
-                <label>Emergency Contact City:
-                    <input type="text" value={emergencyContactCity} onChange={(e) => setEmergencyContactCity(e.target.value)} />
-                </label>
-                <label>Emergency Contact State:
-                    <input type="text" value={emergencyContactState} onChange={(e) => setEmergencyContactState(e.target.value)} />
-                </label>
-                <label>Emergency Contact Zip Code:
-                    <input type="text" value={emergencyContactZipCode} onChange={(e) => setEmergencyContactZipCode(e.target.value)} />
-                </label>
-                <label>Emergency Contact Country:
-                    <input type="text" value={emergencyContactCountry} onChange={(e) => setEmergencyContactCountry(e.target.value)} />
-                </label>
-
-                <button type="submit">Update Profile</button>
+                <button type="submit" className="btn btn-primary mt-3">Update Profile</button>
             </form>
         </div>
     );
