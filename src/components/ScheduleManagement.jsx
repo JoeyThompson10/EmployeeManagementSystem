dddaa893353a0a9bad95540224786401a7e0b2b6
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2023-12-07 20:24:22 -0500

    Made calendar look a little better

--- src/components/ScheduleManagement.jsx
+++ src/components/ScheduleManagement.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect } from 'react';
 import Calendar from 'react-calendar';
-import { getEmployee } from '../MongoDbClient';
 
 const _id = localStorage.getItem('username');
 
 const ScheduleManagement = () => {
+    const tileClassName = ({ date }) => {
+        const isCurrentDay = date.getDate() === new Date().getDate();
+        const isDifferentMonth = date.getMonth() !== new Date().getMonth();
+        
+        if (isCurrentDay) {
+            return 'current-day';
+          } else if (isDifferentMonth) {
+            return 'different-month-day';
+          }
+        return '';
+      };
     return (
         <div>
             <h2>Schedule Management</h2>
             <h3>User: {_id}</h3>
             <div style={{ maxWidth: '600px', margin: 'auto', textAlign: 'center', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '20px', borderRadius: '8px' }}>
-            <Calendar style={{ width: '100%' }}/>
+            <Calendar 
+                style={{ width: '100%' }}
+                tileClassName={tileClassName}
+            />
             </div>
         </div>
     );
