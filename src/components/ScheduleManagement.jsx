48c467840122a5c80e35b5830fa8ebb65148feb1
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2023-12-07 20:14:01 -0500

    Added calendar display to ScheduleManagement tab

--- src/components/ScheduleManagement.jsx
+++ src/components/ScheduleManagement.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Calendar from 'react-calendar';
+import { getEmployee } from '../MongoDbClient';
+
+const _id = localStorage.getItem('username');
 
 const ScheduleManagement = () => {
     return (
         <div>
             <h2>Schedule Management</h2>
+            <h3>User: {_id}</h3>
+            <div style={{ maxWidth: '600px', margin: 'auto', textAlign: 'center', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '20px', borderRadius: '8px' }}>
+            <Calendar style={{ width: '100%' }}/>
+            </div>
         </div>
     );
 };
