b9b5eeb673ddc2a40d3a377f0a8bb53c19740669
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2023-12-08 18:19:43 -0500

    ScheduleManagement tab now displays full schedule of logged in user
    This code is probably going to be moved to the ViewSchedules tab and the ScheduleManagement tab will have more editing features

--- src/components/ScheduleManagement.jsx
+++ src/components/ScheduleManagement.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
 import { getEmployee } from '../MongoDbClient';
+import { DisplayEmployees } from '../MongoDbClient';
 
 const _id = localStorage.getItem('username');
 
@@ -8,34 +9,47 @@ const ScheduleManagement = () => {
     const [schedule, setSchedule] = useState([]);
     
     useEffect(() => {
-        // Fetch the user's schedule from the server using an API call
-        getEmployee(_id)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => setSchedule(data.schedule))
-            .catch(error => console.error('Error fetching schedule:', error));
+        async function fetchData() {
+            // Retrieve the employee with the matching username
+            const employee = await getEmployee(_id);
+    
+            // Check if the employee exists and has a schedule
+            if (employee && employee.schedule) {
+                setSchedule(employee.schedule);
+                console.log(`Schedule for ${_id}: ${JSON.stringify(employee.schedule)}`);
+            } else {
+                console.log(`No schedule found for ${_id}`);
+            }
+        }
+        fetchData();
     }, [_id]);
 
     const tileClassName = ({ date }) => {
         const dateString = date.toISOString().split('T')[0]; // Convert date to string in "YYYY-MM-DD" format
-        const isScheduled = schedule.includes(dateString);
-        const isCurrentDay = date.getDate() === new Date().getDate();
-        const isDifferentMonth = date.getMonth() !== new Date().getMonth();
+        const isCurrentYear = date.getFullYear() === new Date().getFullYear(); // Check if the date is in the current year
+        const isCurrentMonth = isCurrentYear && date.getMonth() === new Date().getMonth(); // Check if the date is in the current month
+        const isCurrentDay = isCurrentMonth && date.getDate() === new Date().getDate();
+        const dayOfWeek = date.getDay(); // 0 for Sunday, 1 for Monday, ..., 6 for Saturday
 
-        if (isScheduled) {
-            return 'scheduled-day';
-        } else if (isCurrentDay) {
+        // Check if the current date is a Saturday or Sunday
+        const isWeekend = dayOfWeek === 0 || dayOfWeek === 6;
+
+        // Check if the current date is within the range of the first and last day of the schedule
+        const isWithinScheduleRange = schedule.length > 0 && date >= new Date(schedule[0]) && date <= new Date(schedule[schedule.length - 1]);
+
+        const isWithinSchedule = schedule.some(day => {
+            const scheduleDate = new Date(day).toISOString().split('T')[0];
+            return scheduleDate === dateString;
+        });
+
+        if (isCurrentDay && isCurrentYear) {
             return 'current-day';
-        } else if (isDifferentMonth) {
-            return 'different-month-day';
+        } else if ((isWithinSchedule || isWithinScheduleRange) && !isWeekend) {
+            return 'within-schedule';
         } else {
             return '';
         }
-      };
+    };
     return (
         <div>
             <h2>Schedule Management</h2>
