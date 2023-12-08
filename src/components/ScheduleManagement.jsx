74b45c63ca0830b536e4b37ce558d7b2f3c0085c
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2023-12-08 16:59:45 -0500

    Schedule management is really close to displaying proper schedule. Error with getEmployee function

--- src/components/ScheduleManagement.jsx
+++ src/components/ScheduleManagement.jsx
@@ -1,19 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
+import { getEmployee } from '../MongoDbClient';
 
 const _id = localStorage.getItem('username');
 
 const ScheduleManagement = () => {
+    const [schedule, setSchedule] = useState([]);
+    
+    useEffect(() => {
+        // Fetch the user's schedule from the server using an API call
+        getEmployee(_id)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => setSchedule(data.schedule))
+            .catch(error => console.error('Error fetching schedule:', error));
+    }, [_id]);
+
     const tileClassName = ({ date }) => {
+        const dateString = date.toISOString().split('T')[0]; // Convert date to string in "YYYY-MM-DD" format
+        const isScheduled = schedule.includes(dateString);
         const isCurrentDay = date.getDate() === new Date().getDate();
         const isDifferentMonth = date.getMonth() !== new Date().getMonth();
-        
-        if (isCurrentDay) {
+
+        if (isScheduled) {
+            return 'scheduled-day';
+        } else if (isCurrentDay) {
             return 'current-day';
-          } else if (isDifferentMonth) {
+        } else if (isDifferentMonth) {
             return 'different-month-day';
-          }
-        return '';
+        } else {
+            return '';
+        }
       };
     return (
         <div>
