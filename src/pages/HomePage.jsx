84264245518026a41c880df00c547abc8fb06e01
zkhan1994 <zeshan.khan1994@outlook.com>
2023-12-09 17:56:49 -0500

    Styling mods

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
 
 function HomePage() {
   const navigate = useNavigate();
 
   return (
-    <div className="home-page">
-      <h1>Home Page</h1>
-      <h1>
+    <div className="home-page container my-5">
+      <h1 className="text-center mb-3">Home Page</h1>
+      <h1 className="text-center mb-4">
         Welcome to the Employee Management System!
       </h1>
-      <p >
+      <p className="text-center lead mb-4">
         This project was created for Kennesaw State University's Cloud Software Development class (SWE 4633) by Imani Allen, Alejandro Bhagwandeen, Zeshan Khan, Carter Ray, and Joseph Thompson.
       </p>
-      <button onClick={() => navigate("/login")}>Login</button>
+      <div className="d-flex justify-content-center">
+        <button onClick={() => navigate("/login")} className="btn btn-primary">Login</button>
+      </div>
     </div>
   );
 }
