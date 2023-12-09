65e19612cc0bc5c232a9d2dcd03c43acd12a4afd
zkhan1994 <zeshan.khan1994@outlook.com>
2023-12-08 20:33:31 -0500

    Dashboard Revamp

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react';
-import { Tabs, TabList, Tab, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AttendanceTracking from '../components/AttendanceTracking';
 import DocumentManagement from '../components/DocumentManagement';
@@ -17,97 +16,116 @@ import ScheduleManagement from '../components/ScheduleManagement';
 import TaskManagement from '../components/TaskManagement';
 import ViewSchedules from '../components/ViewSchedules';
 
-
 function Dashboard() {
-  const isManager = localStorage.getItem('isManager');
+  const [activeTab, setActiveTab] = useState('home');
+  const isManager = localStorage.getItem('isManager') === 'true';
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case 'home':
+        return (
+
+          <div>
+            <h2 className="text-center my-4">Welcome to Your Dashboard</h2>
+
+            <p className="lead text-center">
+              Here's where your journey begins each day. Navigate through the tabs to manage your tasks, view schedules, and more.
+            </p>
+            <div className="lead text-center">
+              <p>📅 Check your schedule in 'View Schedules'</p>
+              <p>📝 Request time off in 'Request Time Off'</p>
+              <p>🛠 Manage your profile in 'Profile Management'</p>
+            </div>
+          </div>
+        );
+      case 'profileManagement':
+        return <ProfileManagement />;
+
+      case 'requestTimeOff':
+        return <RequestTimeOff />;
+      case 'viewSchedules':
+        return <ViewSchedules />;
+      case 'feedback':
+        return <FeedbackForm />;
+      case 'attendanceTracking':
+        return <AttendanceTracking />;
+      case 'taskManagement':
+        return <TaskManagement />;
+      case 'expenseClaims':
+        return <ExpenseClaims />;
+      case 'scheduleManagement':
+        return isManager ? <ScheduleManagement /> : null;
+      case 'payrollManagement':
+        return isManager ? <PayrollManagement /> : null;
+      case 'employeePerformance':
+        return isManager ? <EmployeePerformance /> : null;
+      case 'leaveManagement':
+        return isManager ? <LeaveManagement /> : null;
+      case 'hiringOnboarding':
+        return isManager ? <HiringOnboarding /> : null;
+      case 'employeeManagement':
+        return isManager ? <EmployeeManagement /> : null;
+      default:
+        return null;
+    }
+  };
 
   return (
-    <div className="dashboard">
-      <Tabs>
-        <TabList>
-          {/* All Users */}
-          <Tab>Home</Tab>
-          <Tab>Profile Management</Tab>
-          <Tab>Document Access and Management</Tab>
-          <Tab>Request Time Off</Tab>
-          <Tab>View Schedules</Tab>
-          <Tab>Feedback</Tab>
+    <div className="container">
+      <ul className="nav nav-tabs">
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'home' ? 'active' : ''}`} onClick={() => setActiveTab('home')}>Home</button>
+        </li>
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'profileManagement' ? 'active' : ''}`} onClick={() => setActiveTab('profileManagement')}>Profile Management</button>
+        </li>
 
-          {/* Employee Stuff */}
-          <Tab>Attendance Tracking</Tab>
-          <Tab>Task Management</Tab>
-          <Tab>Expense Claims</Tab>
+        {/* More tabs follow the same pattern */}
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'requestTimeOff' ? 'active' : ''}`} onClick={() => setActiveTab('requestTimeOff')}>Request Time Off</button>
+        </li>
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'viewSchedules' ? 'active' : ''}`} onClick={() => setActiveTab('viewSchedules')}>View Schedules</button>
+        </li>
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'feedback' ? 'active' : ''}`} onClick={() => setActiveTab('feedback')}>Feedback</button>
+        </li>
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'attendanceTracking' ? 'active' : ''}`} onClick={() => setActiveTab('attendanceTracking')}>Attendance Tracking</button>
+        </li>
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'taskManagement' ? 'active' : ''}`} onClick={() => setActiveTab('taskManagement')}>Task Management</button>
+        </li>
+        <li className="nav-item">
+          <button className={`nav-link ${activeTab === 'expenseClaims' ? 'active' : ''}`} onClick={() => setActiveTab('expenseClaims')}>Expense Claims</button>
+        </li>
+        {isManager && (
+          <>
+            <li className="nav-item">
+              <button className={`nav-link ${activeTab === 'scheduleManagement' ? 'active' : ''}`} onClick={() => setActiveTab('scheduleManagement')}>Schedule Management</button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${activeTab === 'payrollManagement' ? 'active' : ''}`} onClick={() => setActiveTab('payrollManagement')}>Payroll Management</button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${activeTab === 'employeePerformance' ? 'active' : ''}`} onClick={() => setActiveTab('employeePerformance')}>Employee Performance Monitoring</button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${activeTab === 'leaveManagement' ? 'active' : ''}`} onClick={() => setActiveTab('leaveManagement')}>Leave Management</button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${activeTab === 'hiringOnboarding' ? 'active' : ''}`} onClick={() => setActiveTab('hiringOnboarding')}>Hiring and Onboarding</button>
+            </li>
+            <li className="nav-item">
+              <button className={`nav-link ${activeTab === 'employeeManagement' ? 'active' : ''}`} onClick={() => setActiveTab('employeeManagement')}>Employee Management</button>
+            </li>
+          </>
+        )}
+      </ul>
 
-          {/* Manager Stuff */}
-          {isManager==='true' && <Tab>Schedule Management</Tab>}
-          {isManager==='true' && <Tab>Payroll Management</Tab>}
-          {isManager==='true' && <Tab>Empoyee Performance Monitoring</Tab>}
-          {isManager==='true' && <Tab>Leave Management</Tab>}
-          {isManager==='true' && <Tab>Hiring and Onboarding</Tab>}
-          {isManager==='true' && <Tab>Employee Management</Tab>}
-        </TabList>
-
-        <TabPanel>
-  <div className="homepage-panel">
-    <h2>Welcome to Your Dashboard</h2>
-    <p>Here's where your journey begins each day. Navigate through the tabs to manage your tasks, view schedules, and more.</p>
-    <div className="quick-info">
-      <p>📅 Check your schedule in 'View Schedules'</p>
-      <p>📄 Access and manage documents in 'Document Access and Management'</p>
-      <p>🛠 Manage your profile in 'Profile Management'</p>
-    </div>
-    {/* You can add an image or icon here if relevant */}
-    {/* <img src="welcome-image.jpg" alt="Welcome" className="welcome-image" /> */}
-  </div>
-</TabPanel>
-
-
-        <TabPanel>
-          <ProfileManagement />
-        </TabPanel>
-        <TabPanel>
-          <DocumentManagement />
-        </TabPanel>
-        <TabPanel>
-          <RequestTimeOff />
-        </TabPanel>
-        <TabPanel>
-          <ViewSchedules />
-        </TabPanel>
-        <TabPanel>
-          <FeedbackForm />
-        </TabPanel>
-
-        <TabPanel>
-          <AttendanceTracking />
-        </TabPanel>
-        <TabPanel>
-          <TaskManagement />
-        </TabPanel>
-        <TabPanel>
-          <ExpenseClaims />
-        </TabPanel>
-
-        {isManager==='true' && <TabPanel>
-          <ScheduleManagement />
-        </TabPanel>}
-        {isManager==='true' && <TabPanel>
-          <PayrollManagement />
-        </TabPanel>}
-        {isManager==='true' && <TabPanel>
-          <EmployeePerformance />
-        </TabPanel>}
-        {isManager==='true' && <TabPanel>
-          <LeaveManagement />
-        </TabPanel>}
-        {isManager==='true' && <TabPanel>
-          <HiringOnboarding />
-        </TabPanel>}
-        {isManager==='true' && <TabPanel>
-          <EmployeeManagement />
-        </TabPanel>}
-
-      </Tabs>
+      <div className="tab-content">
+        {renderTabContent()}
+      </div>
     </div>
   );
 }
