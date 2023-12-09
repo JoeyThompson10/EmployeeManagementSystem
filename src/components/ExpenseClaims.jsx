8e99162bb2ab6efff493cba5abfd632e458b58ea
imani <imaniallen@Imanis-MacBook-Pro.local>
2023-12-09 15:57:52 -0500

    Setting up form for Expense Claim Page

--- src/components/ExpenseClaims.jsx
+++ src/components/ExpenseClaims.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 const ExpenseClaims = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        window.alert('Your expense claim has been submitted.');
+    };
+
+
     return (
-        <div>
+        <div className = 'expense claim form'>
             <h2>Expense Claims</h2>
+
+            <form onSubmit={handleSubmit} className='expense claim form-2' action="/action_page.php">
+                <div className='expense-submission'>
+                    <label htmlFor="name" className="form=label">Name:</label>
+                    <input type="text" name="name" id="name" className="form-control" required />
+                </div>
+
+                <div className='expense-submission'>
+                    <label htmlFor="amount" className="form=label">Please enter amount spent:</label>
+                    <input type="text" name="amount" id="amount" className="form-control" required />
+                </div>
+
+                <div className='expense-submission'>
+                    <label htmlFor="description" className="form=label">Please enter description on what money was used on:</label>
+                    <input type="text" name="description" id="description" className="form-control" required />
+                </div>
+
+                <div className='expense-submission'>
+                    <label htmlFor="receipt" className="form=label">Please provide receipt:</label>
+                    <input type="file" name="description" id="description" className="form-control" required />
+                </div>
+
+                <input type="submit" value="Submit" className="btn btn-primary" />
+
+            </form>
         </div>
     );
 };
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,9 +5,11 @@ function AboutPage() {
   return (
     <div className="about-page">
       <h1>Contributors</h1>
-      <p>Created by: Alejandro Bhagwandeen, Zeshan Khan, Carter Ray, and Joseph Thompson, Imani Allen <br />
-        Kennesaw State University<br />
-        Cloud Software Development (SWE 4633 Section 01)
+      <p>
+        Created by: Alejandro Bhagwandeen, Zeshan Khan, Carter Ray, Joseph Thompson, and Imani Allen <br />
+        School: Kennesaw State University <br />
+        Class: Cloud Software Development (SWE 4633 Section 01) <br />
+        Year: 2023
       </p>
       <h2>About the Employee Management System</h2>
       <p>
@@ -22,11 +24,12 @@ function AboutPage() {
         <li><strong>Schedule Management</strong> Allows management to review time off requests</li>
         <li><strong>Feedback</strong> Allows employees to submit feedback and suggestions to management</li>
         <li><strong>Task Management</strong> Allows managers to se the tasks assigned to all employees</li>
+        <li><strong>Expense Claims</strong> Allows employees to upload any company expenses</li>
       </ul>
       <h3>Code Repository:</h3>
       <p>
         To view the source code for this project, please click the following link:
-        <a href= "https://github.com/JoeyThompson10/EmployeeManagementSystem">!!LINK TO SOURCE CODE HERE!!</a>
+        <a href= "https://github.com/JoeyThompson10/EmployeeManagementSystem"> LINK TO SOURCE CODE HERE </a>
       </p>
     </div>
   );
