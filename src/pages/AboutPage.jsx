164d28aec1d6225bc8f498e922d6cfcb252cefca
imani <imaniallen@Imanis-MacBook-Pro.local>
2023-12-09 16:53:05 -0500

    Creating documentation page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,13 +4,6 @@ import '../css/global.css';
 function AboutPage() {
   return (
     <div className="about-page">
-      <h1>Contributors</h1>
-      <p>
-        Created by: Alejandro Bhagwandeen, Zeshan Khan, Carter Ray, Joseph Thompson, and Imani Allen <br />
-        School: Kennesaw State University <br />
-        Class: Cloud Software Development (SWE 4633 Section 01) <br />
-        Year: 2023
-      </p>
       <h2>About the Employee Management System</h2>
       <p>
         This application is an app designed for managers and employees at a company. 
@@ -26,11 +19,6 @@ function AboutPage() {
         <li><strong>Task Management</strong> Allows managers to se the tasks assigned to all employees</li>
         <li><strong>Expense Claims</strong> Allows employees to upload any company expenses</li>
       </ul>
-      <h3>Code Repository:</h3>
-      <p>
-        To view the source code for this project, please click the following link:
-        <a href= "https://github.com/JoeyThompson10/EmployeeManagementSystem"> LINK TO SOURCE CODE HERE </a>
-      </p>
     </div>
   );
 }
---
+++ src/pages/Documentation.jsx
@@ -0,0 +1,33 @@
+import React from "react"; 
+
+const Documentation = () => {
+    return (
+        <div className="documentation-page">
+            <h1>Contributors</h1>
+                <p>
+                    Created by: Alejandro Bhagwandeen, Zeshan Khan, Carter Ray, Joseph Thompson, and Imani Allen <br />
+                    School: Kennesaw State University <br />
+                    Class: Cloud Software Development (SWE 4633 Section 01) <br />
+                    Year: 2023
+                </p>
+            <h2>Project Documentation</h2>
+            <ul>
+                <li>
+                    <strong>Project Proposal: </strong> 
+                    <a href="file:///Users/imaniallen/Downloads/ProjectProposal.pdf" download>Project Proposal</a>
+                </li>
+                <li>
+                    <strong>ER Diagram: </strong>
+                    <a href="file:///Users/imaniallen/Downloads/DatabaseAndWebImplementation.pdf" download>ER Diagram</a>
+                </li>
+            </ul>
+            <h3>Code Repository:</h3>
+                <p>
+                    To view the source code for this project, please click the following link:
+                    <a href= "https://github.com/JoeyThompson10/EmployeeManagementSystem"> LINK TO SOURCE CODE HERE </a>
+                </p>
+        </div>
+    );
+};
+
+export default Documentation;
