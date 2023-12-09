3ccd476977f8911f737df5f3f4d2ba83ec0c7eb1
zkhan1994 <zeshan.khan1994@outlook.com>
2023-12-09 17:50:22 -0500

    Contact and Terms page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
-import '../css/global.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
 
 function ContactPage() {
   return (
-    <div className="contact-page">
-      <h1>Contact</h1>
-      <p>Welcome to the contact page!</p>
+    <div className="contact-page container my-5">
+      <h1 className="text-center mb-4">Contact Us</h1>
+      <p className="text-center lead">Welcome to the contact page! If you have any questions or feedback, feel free to reach out to us.</p>
+      
+      <div className="row">
+        <div className="contact-details col-md-6">
+          <h2>Get in Touch</h2>
+          <p>For general inquiries, you can reach us at:</p>
+          <ul>
+            <li>Email: contact@example.com</li>
+            <li>Phone: (123) 456-7890</li>
+            <li>Address: 123 Fake Street, Springfield, USA</li>
+          </ul>
+        </div>
+
+        <div className="contact-form col-md-6">
+          <h2>Contact Form</h2>
+          <form>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">Name:</label>
+              <input type="text" id="name" name="name" className="form-control" placeholder="Your name" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email:</label>
+              <input type="email" id="email" name="email" className="form-control" placeholder="Your email" />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="message" className="form-label">Message:</label>
+              <textarea id="message" name="message" className="form-control" placeholder="Your message"></textarea>
+            </div>
+            <button type="submit" className="btn btn-primary">Send Message</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
