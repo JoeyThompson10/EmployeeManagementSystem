89f70b06271e7a7ad917af5bda83d9a93e28a84f
imani <imaniallen@Imanis-MacBook-Pro.local>
2023-12-09 14:55:48 -0500

    Updating AboutPage

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,15 +4,29 @@ import '../css/global.css';
 function AboutPage() {
   return (
     <div className="about-page">
-      <h1>About This App</h1>
-      <p>This application is a minimalistic template designed to help kickstart your next React project with Vite. It incorporates basic yet essential features such as routing, navigation, and tabs to demonstrate a typical app structure.</p>
-      <h2>Packages Used</h2>
+      <h1>Contributors</h1>
+      <p1>Created by: Alejandro Bhagwandeen, Zeshan Khan, Carter Ray, and Joseph Thompson, Imani Allen,</p1>
+      <p2>Kennesaw State University</p2>
+      <p3>Cloud Software Development (SWE 4633 Section 01) </p3>
+      <h2>About the Employee Management System</h2>
+      <p>
+        This application is an app designed for managers and employees at a company. 
+        Use of this app allows for better organization of manager and employee needs such as viewing schedules, managing their profile, and even requesting time off.
+      </p>
+      <h3>How it works</h3>
       <ul>
-        <li><strong>react-router-dom:</strong> Enables the routing functionalities within the application, allowing for seamless navigation between different components/pages.</li>
-        <li><strong>react-tabs:</strong> An accessible and easy-to-use package to implement tabs within the app, enhancing the user experience and content organization.</li>
+        <li><strong>Profile Management</strong> Allows users to update personal information such as name, email, address and phone number</li>
+        <li><strong>Request Time Off</strong> Allows employees to request time off</li>
+        <li><strong>View Schedule</strong> Allows users to view work schedules</li>
+        <li><strong>Schedule Management</strong> Allows management to review time off requests</li>
+        <li><strong>Feedback</strong> Allows employees to submit feedback and suggestions to management</li>
+        <li><strong>Task Management</strong> Allows managers to se the tasks assigned to all employees</li>
       </ul>
-      <h3>Customizable Styling</h3>
-      <p>All styles are easily customizable through the CSS file. The use of CSS variables ensures that you can quickly change the app's color scheme, text styles, and other visual elements from a central location. Feel free to adapt and extend the styles to match your specific needs and brand identity.</p>
+      <h3>Ccode Repository:</h3>
+      <p>
+        To view the source code for this project, please click the following link:
+        <a href= "https://github.com/JoeyThompson10/EmployeeManagementSystem"></a>
+      </p>
     </div>
   );
 }
