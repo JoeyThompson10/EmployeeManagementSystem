eb000d93982f21ceea1d69555c2449961986d2cf
zkhan1994 <zeshan.khan1994@outlook.com>
2023-12-09 20:14:24 -0500

    Styling

--- src/pages/TermsPage.jsx
+++ src/pages/TermsPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 function TermsPage() {
   return (
