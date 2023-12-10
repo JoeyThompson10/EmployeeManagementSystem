6b7469d40e7bfcb4e2bbadbf46f3773ac469d36b
carter-ktb21 <144170194+carter-ktb21@users.noreply.github.com>
2023-12-09 20:15:10 -0500

    Added videos to Onboarding

--- src/components/HiringOnboarding.jsx
+++ src/components/HiringOnboarding.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 const HiringOnboarding = () => {
     return (
         <div>
-            <h2>Onboarding</h2>
+            <h1>Onboarding</h1>
+            <h2>The Videos Listed Below Represent Our Team Values. Please Watch Them.</h2>
+            <h3 style={{textAlign:'center'}}>How To Code:</h3>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <iframe
+                style={{ border: 'none' }}
+                width="420"
+                height="315"
+                src="https://youtube.com/embed/QFaFIcGhPoM"
+                title="YouTube Video"
+            ></iframe>
+            </div>
+            <br />
+            <h3 style={{textAlign:'center'}}>How To Wear Clothes:</h3>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <iframe
+                style={{ border: 'none' }}
+                width="420"
+                height="315"
+                src="https://youtube.com/embed/7OlfCL5nvVU"
+                title="YouTube Video"
+            ></iframe>
+            </div>
+            <br />
+            <h3 style={{textAlign:'center'}}>How To Succeed:</h3>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <iframe
+                style={{ border: 'none' }}
+                width="420"
+                height="315"
+                src="https://youtube.com/embed/dQw4w9WgXcQ"
+                title="YouTube Video"
+            ></iframe>
+            </div>
         </div>
     );
 };
